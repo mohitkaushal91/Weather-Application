40cd8a7f3a34e5d234633c20002f2734a0eb2ba8
Mohit Kumar <mohitkaushal91@gmail.com>
2018-07-22 15:41:03 +0530

    Update weatherLatLong.php

--- weatherLatLong.php
+++ weatherLatLong.php
@@ -4,14 +4,10 @@ $lat = $_POST['latitude'];
 $long = $_POST['longitude'];
 
 	$baseurl = 'http://api.openweathermap.org/data/2.5/weather';
-	$finalurl = $baseurl . "?lat=" . $lat . "&lon=" . $long . "&APPID=5ab905a423f6d45914490a574a80284d";
+	$finalurl = $baseurl . "?lat=" . $lat . "&lon=" . $long . "&APPID=Your-API-Key-Here";
 
 
 	$response = json_decode(file_get_contents($finalurl), true);
-
-	//$temp = $response['main']['temp'];
-
-	//$temp = ($temp-273.15);
 
 	echo json_encode($response);
 
